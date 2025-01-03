402a2006a216ea7ba9f30005db989847722fef76
Kyran Koehler <kyran.koehler@student.rocva.nl>
2025-01-03 11:11:09 +0100

    opdracht 5 is af chill

--- resources/views/snoep/index.blade.php
+++ resources/views/snoep/index.blade.php
@@ -11,31 +11,34 @@
   <div class="container" style="margin:40px;">
   <h1 class="display-3">Snoep</h1>
   @guest
-            <a href="{{ route('login') }}" class="btn btn-primary">Inloggen</a>
-        @endguest
+      <a href="{{ route('login') }}" class="btn btn-primary">Inloggen</a>
+  @endguest
 
-        <!-- Toon Uitlogknop als de gebruiker wel ingelogd is -->
-        @auth
-            <form action="{{ route('logout') }}" method="POST" style="display:inline;">
-                @csrf
-                <button type="submit" class="btn btn-danger">Uitloggen</button>
-            </form>
-        @endauth
- 
-<div>
-@auth
-  <a href="/snoep/create" class="btn btn-primary mb-3">Add Snoep</a>
+  <!-- Toon Uitlogknop als de gebruiker wel ingelogd is -->
+  @auth
+      <form action="{{ route('logout') }}" method="POST" style="display:inline;">
+          @csrf
+          <button type="submit" class="btn btn-danger">Uitloggen</button>
+      </form>
   @endauth
-</div> 
-    <table class="table">
+  <div>
+      <!-- Maak de knop voor het aanmaken van snoep zichtbaar voor alle ingelogde gebruikers -->
+      @auth
+          <a href="/snoep/create" class="btn btn-primary mb-3">Add Snoep</a>
+      @endauth
+  </div> 
+
+  <table class="table">
       <thead class="thead-light">
           <tr>
             <th>ID</th>
             <th>Snoep naam</th>
             <th>Prijs snoep</th>
             <th>Updated at</th>
-            <th>Update</th>
-            <th>Delete</th>
+            @role('admin')
+                <th>Update</th>
+                <th>Delete</th>
+            @endrole
           </tr>
       </thead>
       <tbody>
@@ -45,20 +48,19 @@
               <td>{{$snoep->naam}} </td>
               <td>{{$snoep->value}}</td>
               <td>{{$snoep->updated_at}}</td>
-              @auth
-              <td><a href="/snoep/edit/{{$snoep->id}}" class="btn btn-primary">Edit</a></td>
-              <td>
-  <form action="/snoep/destroy/{{$snoep->id}}" method="post">
-    @csrf
-    <button onclick="return confirm('Are you sure?')" class="btn btn-danger" type="submit">Delete</button>
-  </form>
-  @endauth
-</td>
+              @role('admin')
+                  <td><a href="/snoep/edit/{{$snoep->id}}" class="btn btn-primary">Edit</a></td>
+                  <td>
+                      <form action="/snoep/destroy/{{$snoep->id}}" method="post">
+                          @csrf
+                          <button onclick="return confirm('Are you sure?')" class="btn btn-danger" type="submit">Delete</button>
+                      </form>
+                  </td>
+              @endrole
           </tr>
           @endforeach
       </tbody>
-    </table>
+  </table>
   </div>
-
 </body>
 </html>
